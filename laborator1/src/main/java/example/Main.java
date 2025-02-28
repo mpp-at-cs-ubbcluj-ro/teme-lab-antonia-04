858bcafe1354f97df1f92cae89fb00a2b306ba0f
Antonia <90861581+antonia-04@users.noreply.github.com>
2025-02-28 12:30:45 +0200

    update laborator1

--- laborator1/src/main/java/example/Main.java
+++ laborator1/src/main/java/example/Main.java
@@ -4,6 +4,6 @@ package example;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.printf("Hello and welcome! Stii sa folosesti github :)");
+        System.out.printf("Hello and welcome! ");
     }
 }
